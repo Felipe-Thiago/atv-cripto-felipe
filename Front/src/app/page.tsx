c1615c1f7f055f3a7a1c518c57a80399cb6ae7a2
Felipe Thiago <felipethiagodasilva@gmail.com>
2025-05-19 17:08:42 -0300

    fix: merge fix

--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -1,12 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-<<<<<<< HEAD
 import { useEffect } from "react";
-=======
-import { useAuth } from "@/context/AuthContext";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
->>>>>>> 05636f1bb5315593cd48a8bfabedd4dae54cf621
 
 export default function App() {
   const router = useRouter();
